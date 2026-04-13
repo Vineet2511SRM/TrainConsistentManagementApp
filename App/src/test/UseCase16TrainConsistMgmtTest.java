d87bed3f0140cde27338aef44829844341f5f0c3
VINEET2511SRM <emperorvineet7@gmail.com>
2026-04-13 16:02:46 +0530

    VINEET SETH ADDED UC16

---
+++ App/src/test/UseCase16TrainConsistMgmtTest.java
@@ -0,0 +1,77 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UseCase16TrainConsistMgmtTest {
+
+    private int[] bubbleSort(int[] arr) {
+
+        int[] result = arr.clone();
+
+        for (int i = 0; i < result.length - 1; i++) {
+
+            for (int j = 0; j < result.length - i - 1; j++) {
+
+                if (result[j] > result[j + 1]) {
+
+                    int temp = result[j];
+                    result[j] = result[j + 1];
+                    result[j + 1] = temp;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    @Test
+    void testSort_BasicSorting() {
+
+        int[] input = {72, 56, 24, 70, 60};
+
+        int[] expected = {24, 56, 60, 70, 72};
+
+        assertArrayEquals(expected, bubbleSort(input));
+    }
+
+    @Test
+    void testSort_AlreadySortedArray() {
+
+        int[] input = {24, 56, 60, 70, 72};
+
+        int[] expected = {24, 56, 60, 70, 72};
+
+        assertArrayEquals(expected, bubbleSort(input));
+    }
+
+    @Test
+    void testSort_DuplicateValues() {
+
+        int[] input = {72, 56, 56, 24};
+
+        int[] expected = {24, 56, 56, 72};
+
+        assertArrayEquals(expected, bubbleSort(input));
+    }
+
+    @Test
+    void testSort_SingleElementArray() {
+
+        int[] input = {50};
+
+        int[] expected = {50};
+
+        assertArrayEquals(expected, bubbleSort(input));
+    }
+
+    @Test
+    void testSort_AllEqualValues() {
+
+        int[] input = {40, 40, 40};
+
+        int[] expected = {40, 40, 40};
+
+        assertArrayEquals(expected, bubbleSort(input));
+    }
+}
